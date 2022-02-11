0e9b4d07c2fa39acc91d9b18a8b06fc2b2d7b731
xone46 <achraflahcen46@gmail.com>
2022-02-11 15:35:50 +0100

    repare client

---
+++ client/vue.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    configureWebpack: {
+        devServer: {
+            proxy: {
+                '^/api': {
+                    target: 'http://localhost:3000',
+                    changeOrigin: true,
+                    secure:false,
+                    pathRewrite: {'^/api': '/api'},
+                    logLevel: 'debug' 
+                },
+
+            },
+
+        },
+    },
+}
